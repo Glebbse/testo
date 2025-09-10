afc6c93499baa413dae08b8a23f8f15ff186ed97
Glebbse <pearsebrosnan7@gmail.com>
2025-09-10 19:20:39 +0300

    wrapped code in main body into functions

--- open_weather_API.py
+++ open_weather_API.py
@@ -14,35 +14,30 @@ def create_parser():
     parser.add_argument("-c", "--city", required=True, help='City name. Example: London')
     return parser
 
-result = []
-if __name__ == "__main__":
-    parse = create_parser()
-    namespace = parse.parse_args(sys.argv[1:])
-    url_geo = f"http://api.openweathermap.org/geo/1.0/direct?q={namespace.city}&limit=1&appid={api}"
+def get_geo(*, city:str):
+    url_geo = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={api}"
     try:
-        response_geo = requests.get(url=url_geo)
+        response_geo = requests.get(url=url_geo, timeout=10)
         response_geo.raise_for_status()
-
     except requests.exceptions.RequestException as err:
-        print(f"Response error {err}")
-    except KeyError as er:
-        print(f"Key error {er}")
+        sys.exit(f"Coordinates request error {err}")
     geo_data = response_geo.json()
     if not geo_data:
-        sys.exit(f"City {namespace.city} hasn't been found.")
+        sys.exit(f"City {city} hasn't been found.")
+    return geo_data[0]["lat"], geo_data[0]["lon"]
 
-    lat, lon = geo_data[0]["lat"], geo_data[0]["lon"]
-    url_weather = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=daily,hourly,minutely&appid={api}"
+def get_weather(*, lat:float, lon: float):
+    url_weather =f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=daily,hourly,minutely&appid={api}"
     try:
-        response_weather = requests.get(url=url_weather, params={"units": "metric", "lang": "ru"})
+        response_weather = requests.get(url=url_weather, params={"units": "metric", "lang": "ru"}, timeout=10)
         response_weather.raise_for_status()
     except requests.exceptions.RequestException as err:
-        print(f"Response error {err}")
-    except KeyError as er:
-        print(f"Key error {er}")
-    print(f"City: {namespace.city}\ntimezone: {response_weather.json()['timezone']}")
-    current = response_weather.json()['current']
+        sys.exit(f"Weather request error {err}")
+    return response_weather.json()
 
+def format_weather(*, city: str, data: dict):
+    result = [f"City/Город: {city}", f"GMT/Часовой пояс: {data['timezone']}"]
+    current = data["current"]
     for i, key in enumerate(current):
         if key == "dt":
             result.append(f"date time: {datetime.fromtimestamp(current.get(key))}")
@@ -54,5 +49,12 @@ if __name__ == "__main__":
             result.append(f"Описание: {current['weather'][0]['description']}")
             continue
         result.append(f"{key}: {current.get(key)}")
+    return "\n".join(result)
 
-    print("\n".join(result))
+
+if __name__ == "__main__":
+    parse = create_parser()
+    namespace = parse.parse_args(sys.argv[1:])
+    lati, long = get_geo(city=namespace.city)
+    weather = get_weather(lat=lati, lon=long)
+    print(format_weather(city=namespace.city, data=weather))
